7b5a1797ef9f182100f73b1458e941435d3a8d87
Aman gupta <117616487+AG6589@users.noreply.github.com>
2026-01-29 14:44:24 +0530

    Configure server to listen on env PORT for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -7,5 +7,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
